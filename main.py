c8c6f8cd1f4a3e132c0488b6694b43a55d62ac35
teoyi <yipengteo@yahoo.com.sg>
2020-08-11 23:36:22 -0700

    Craeted base files and login page structure

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('design.kv')
+
+class LoginScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    pass
+
+if __name__ == "__main__":
+    MainApp().run()
