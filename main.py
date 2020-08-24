af67e6b2f4fca62a165461aba6ba397ec1da71e0
teoyi <yipengteo@yahoo.com.sg>
2020-08-23 20:40:43 -0700

    Added get quote functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+import json, glob
+from pathlib import Path
+import random 
 
 Builder.load_file('design.kv')
 
@@ -43,6 +45,20 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+    
+    def get_quote(self, feel): 
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+
+        available_feelings = [Path(filename).stem for filename in available_feelings]
+
+        if feel in available_feelings: 
+            with open(f"quotes/{feel}.txt") as file: 
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling"
+
 class MainApp(App):
     def build(self):
         return RootWidget()
